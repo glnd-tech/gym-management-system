27d957edb8c4a767453339d3716b84e53b028142
Guillermo B. Galindo S. <gb.galindo.tech@gmail.com>
2026-04-13 12:17:06 -0400

    feat: conexion exitosa de NestJS a PostgreSQL con TypeORM

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    // 1. Cargar las variables del .env
+    ConfigModule.forRoot(),
+
+    // 2. Conexión a la base de datos
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT || '5433', 10),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      autoLoadEntities: true,
+      synchronize: true, // Sincroniza las tablas automáticamente en modo desarrollo
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
